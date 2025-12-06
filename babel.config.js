71b31dcc158a670385f6a10f4de2daa514e52869
Pranjal6955 <negipranjal555@gmail.com>
2025-12-06 19:33:05 +0530

    feat: Integrate react-native-reanimated, update react-native-worklets, and refresh EAS project ID.

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,7 @@ module.exports = function (api) {
     presets: [
       [
         "babel-preset-expo",
-        {
-          // Disable reanimated plugin since we removed react-native-reanimated
-          // Even though it's a transitive dependency, we don't want to use it
-          reanimated: false,
-        },
+
       ],
       "nativewind/babel",
     ],
