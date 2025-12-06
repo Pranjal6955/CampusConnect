486b390d98bfe03cb1e5b3cba4f7fe91ad1dfc70
Pranjal6955 <negipranjal555@gmail.com>
2025-12-06 18:39:32 +0530

    chore: Update environment configuration and ignore local .env files; modify projectId in app.json and adjust Babel preset settings

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      "babel-preset-expo",
+      [
+        "babel-preset-expo",
+        {
+          // Disable reanimated plugin since we removed react-native-reanimated
+          reanimated: false,
+        },
+      ],
       "nativewind/babel",
     ],
   };
